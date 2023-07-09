1cb5e985381279a5198787ac35dbbf112d80eb3e
snepraj2709 <snehaprajapatijnv15@gmail.com>
2023-07-09 14:46:00 +0530

    feat: Explore page added

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,5 +1,35 @@
+import { useAuth } from "../../context/authContext";
+import { usePost } from "../../context/postContext";
+import {Sidebar} from '../../components/sidebar/Sidebar'
+import {Searchbar} from '../../components/search/Searchbar';
+import {PostCard} from '../../components/postCard/PostCard';
+
 export default function Explore(){
-    return(
-        <div>Explore page</div>
-    )
+     const {loginUser}=useAuth();
+    const {isLoading,postState:{allPosts,filterType},postDispatch}=usePost();
+
+  return (
+    <div className="flex">
+      <div className="w-1/4 border ">
+        <aside className="flex-shrink-0 center">
+          <Sidebar />
+        </aside>
+      </div>
+      <div className="w-2/4 border border-gray-700 md:items-center">
+        <Searchbar/>
+
+        {allPosts.length!==0?
+          allPosts.map((post)=>(
+            <div key={post._id}>
+             <PostCard post={post}/>
+             <hr/>
+            </div>
+          )):(<div>No Posts</div>)
+        }
+      </div>
+      <div className="w-1/4 border hidden lg:block">
+        <div className="bg-gray-300">Suggested User</div>
+      </div>
+    </div>
+  );
 }
