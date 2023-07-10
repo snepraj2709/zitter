2c36877ed49845a7de472d222232479dd2d5dea6
snepraj2709 <snehaprajapatijnv15@gmail.com>
2023-07-10 15:16:46 +0530

    bug: Follow user from suggested users list

--- src/components/suggestedUser/SuggestedUser.jsx
+++ src/components/suggestedUser/SuggestedUser.jsx
@@ -5,12 +5,14 @@ import { useNavigate } from 'react-router-dom';
 
 export const SuggestedUser = () => {
   const { userState: { allUsers },followUserHandler } = useUser();
-  const { loginUser } = useAuth();
+  const { loginUser,token } = useAuth();
   const navigate = useNavigate();
 
   const usersNotFollowedByLoginUser = allUsers.filter((user) => {
     return !user.followers.some((follower) => follower.username === loginUser.username);
   });
+
+  console.log('usersNotFollowedByLoginUser',usersNotFollowedByLoginUser)
 
   return (
     <div>
@@ -19,18 +21,18 @@ export const SuggestedUser = () => {
         {usersNotFollowedByLoginUser.length > 0 ? (
           usersNotFollowedByLoginUser.map((user) => (
             <div
-              key={user._id}
+              key={user?.id}
               onClick={() => {
-                navigate(`/profile/${user.username}`);
+                navigate(`/profile/${user?.username}`);
               }}
               className=" flex flex-row items-center justify-center text-white p-2 cursor-pointer w-56 rounded border"
             >
               <UserAvatar user={user} />
               <div className="mt-2 flex flex-col m-1 text-left">
-                <span className="text-sm text-gray-800">{user.firstName + " " + user.lastName}</span>
-                <span className="text-sm text-gray-800">{user.username}</span>
+                <span className="text-sm text-gray-800">{user?.firstName + " " + user?.lastName}</span>
+                <span className="text-sm text-gray-800">{user?.username}</span>
               </div>
-                  <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-2 rounded mr-2" onClick={()=>followUserHandler(_id,token)}>
+                  <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-2 rounded mr-2" onClick={()=>followUserHandler(user?._id,token)}>
                     Follow
                   </button>
             </div>
